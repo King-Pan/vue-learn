ca6b02a6fd14f40e2d98b224151ddc8a7d593ee2
AsiainfoWangzongjie <pwpw1218@qq.com>
2018-09-06 17:48:16 +0800

    vue-login简易教程完成

--- vue-login/src/router/index.js
+++ vue-login/src/router/index.js
@@ -19,19 +19,22 @@ router.beforeEach((to, from, next) => {
   console.log(token)
   if (!token && to.name !== LOGIN_PAGE_NAME) {
     // 未登录且要跳转的页面不是登录页
-
+    console.log('没有登录，并且请求地址不是登录页面')
     next({
       name: LOGIN_PAGE_NAME // 跳转到登录页
     })
   } else if (!token && to.name === LOGIN_PAGE_NAME) {
     // 未登陆且要跳转的页面是登录页
+    console.log('没有登录，并且请求地址是登录页面')
     next() // 跳转
   } else if (token && to.name === LOGIN_PAGE_NAME) {
     // 已登录且要跳转的页面是登录页
+    console.log('已经登录，并且请求地址不是登录页面，跳转到首页')
     next({
       name: 'main' // 跳转到home页
     })
   } else {
+    console.log('其他的直接跳转到需要跳转的路由页面')
     next()
   }
 })
